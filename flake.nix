4de448ab9a0ac1a76ec0ef356eaa4c931be68275
@SakulFlee | Lukas <dev@sakul-flee.de>
2026-05-21 18:52:43 +0200

    Changed nixos pkgs version

--- flake.nix
+++ flake.nix
@@ -1,5 +1,8 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
   outputs = { self, nixpkgs }: {
     nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
       modules = [ ./hosts/vm/configuration.nix ];
