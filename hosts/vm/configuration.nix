5b57b93902c9d39e21a5e0e79f55a2318b8cd0e2
@SakulFlee | Lukas <dev@sakul-flee.de>
2026-05-21 22:13:37 +0200

    Trying with custom theme

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -22,10 +22,17 @@
   boot.loader.grub.useOSProber = false;
 
   # SDDM
+  environment.systemPackages = [
+    (pkgs.catppuccin-sddm.override {
+      flavor = "mocha";
+      accent = "mauve";
+    })
+  ];
   services.displayManager = {
     sddm = {
       enable = true;
       wayland.enable = true;
+      theme = "catppuccin-mocha-mauve";
     };
 
     defaultSession = "hyprland-uwsm";
