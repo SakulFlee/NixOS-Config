a9264a10e94d7d83c5802a350716d04e7e0c12f7
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-21 16:27:41 +0200

    Added hardware config and configured host

---
+++ hosts/vm/configuration.nix
@@ -0,0 +1,28 @@
+{ lib, pkgs, ... }: {
+  imports = [
+    ./hardware.nix
+    ../../shared/kernel.nix
+    ../../shared/display_server/wayland.nix
+    ../../shared/timezone.nix
+    ../../shared/language.nix
+    ../../shared/networkmanager.nix
+    ../../shared/audio/pulsewire.nix
+    ../../shared/packages/_default.nix
+    ../../shared/ssh.nix
+    ../../shared/gpg.nix
+  ];
+
+  # Hostname
+  networking.hostName = "VM";
+
+  # Bootloader
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = false;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+}
+
