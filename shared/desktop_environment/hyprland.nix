2f4137b00de81669eceb8d2b8bd56ceda41d1f7b
@SakulFlee | Lukas <dev@sakul-flee.de>
2026-05-21 18:15:19 +0200

    Added Hyprland

---
+++ shared/desktop_environment/hyprland.nix
@@ -0,0 +1,11 @@
+{
+  imports = [
+    # KiTTY is required by Hyprland!
+    ../packages/kitty.nix
+  ];
+
+  programs.hyprland.enable = true;
+
+  # Hint Electron apps to use Wayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+}
