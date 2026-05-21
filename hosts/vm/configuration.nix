d81e434b9e3ccc7b3c32a40d14a46349e7f0c8df
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-21 17:50:59 +0200

    Updated key paths for gnome and gdm

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -20,8 +20,8 @@
   boot.loader.grub.useOSProber = false;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
 
   # Auto-Login
   services.displayManager.autoLogin = {
