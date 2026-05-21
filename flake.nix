0fe14f3acfe93def1425c79d495e943056768f79
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-21 16:28:07 +0200

    Added main flake

---
+++ flake.nix
@@ -0,0 +1,8 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
+      modules = [ ./hosts/vm/configuration.nix ];
+    }
+  }
+}
