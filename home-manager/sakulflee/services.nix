a0265ec85a5d757014912cfaad226b01823f4f00
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-23 01:18:22 +0200

    fix: adjusted lat/long

--- home-manager/sakulflee/services.nix
+++ home-manager/sakulflee/services.nix
@@ -5,7 +5,7 @@
       After = [ "graphical-session.target" ];
     };
     Service = {
-      ExecStart = "${pkgs.wlsunset}/bin/wlsunset -l 51.196082 -L 6.437104";
+      ExecStart = "${pkgs.wlsunset}/bin/wlsunset -l 51.15 -L 6.5";
     };
     Install = {
       WantedBy = [ "graphical-session.target" ];
