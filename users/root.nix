05e0a949b599c8ffdd1256286e2f7ccfa30b86d3
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-22 21:17:03 +0200

    fix: remove passwd attributes and switch to manual password setting
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- users/root.nix
+++ users/root.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   users.users.root = {
-    passwd = "sha512$6$/8CQ1JQEOl0F8Zta$j4lqp3Ep9X5xnnngZkJEkx2R5MFxWmWVzQq6wT7O7tQ1SK6VEn9wvRqy3u3du7yBl.yq1hFcDW0Pqxaa.Fc3T0";
+    # Password should be set via 'passwd' command for security
   };
 
   home-manager.users.root = { pkgs, ... }: {
