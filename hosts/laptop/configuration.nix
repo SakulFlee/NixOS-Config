3085b5704e4e3523abffc02477cd4aa548716ad6
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-23 04:09:50 +0200

    feat: laptop config

---
+++ hosts/laptop/configuration.nix
@@ -0,0 +1,34 @@
+{ lib, pkgs, ... }: {
+  imports = [
+    ./hardware.nix
+    ../../users/sakulflee.nix
+    ../../users/root.nix
+    ../../shared/_defaults.nix
+  ];
+
+  # Hostname
+  networking.hostName = "Cindry";
+
+  # Bootloader
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/disk/by-uuid/fad2025e-e33e-45a5-af9a-8338cca5ee97";
+  boot.loader.grub.useOSProber = false;
+
+  # SDDM
+  environment.systemPackages = [
+    (pkgs.catppuccin-sddm.override {
+      flavor = "mocha";
+      accent = "mauve";
+    })
+  ];
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      wayland.enable = true;
+      theme = "catppuccin-mocha-mauve";
+    };
+
+    defaultSession = "plasma";
+  };
+}
+
---
+++ hosts/laptop/hardware.nix
@@ -0,0 +1,67 @@
+{ config, lib, pkgs, modulesPath, ... }: {
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/861f305b-5dd5-4500-b602-2e286dca334d";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/2AAA-B9A7";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [{ 
+    device = "/dev/disk/by-uuid/dde0c8d0-6d7c-4cb5-ac79-5075540c8fe1"; 
+    fsType = "swap";
+  }];
+
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # GPU config
+  hardware.graphics = {
+    enable = true;
+    extraPackages = [ pkgs.mesa ];
+  };
+  services.xserver.videoDrivers = [
+    "amdgpu"
+    "nvidia"
+  ];
+  hardware.nvidia = {
+    # Modesetting
+    modesetting.enable = true;
+
+    # Experimental!
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
+
+    # Driver version
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    open = true;
+
+    # Include settings app?
+    nvidiaSettings = true;
+
+    # Prime
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      nvidiaBusId = "PCI:1:0:0";
+      amdgpuBusId = "PCI:5:0:0";
+    };
+  };
+
+  # WiFi Card (prevents speed and reliability issues)
+  boot.extraModprobeConfig = ''
+    options mt7921e disable_aspm=1
+  '';
+  networking.networkmanager.wifi.powersave = false;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
