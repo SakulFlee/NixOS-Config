e129d06b86483d7ddaa6b508683de00637e60501
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-23 02:13:35 +0200

    fix: cleanup

--- home-manager/sakulflee/packages.nix
+++ home-manager/sakulflee/packages.nix
@@ -3,25 +3,7 @@
     htop
     btop
     lmstudio
-    waybar
-
-    # Application Launcher & Tools
-    hyprlauncher
     nnn
-    xfce.thunar
-    fzf
-
-    # System Utilities
-    mako
-    hyprsunset
-    hyprpwcenter
-    hyprshutdown
-    wlogout
-    wlsunset
-    hyprlock
-    hypridle
-
-    # General Software
     discord
     floorp-bin
     vlc
