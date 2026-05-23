2e172b0be41b51bfd22f88775131bac6da24b0c5
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-23 02:13:29 +0200

    fix: removed hyprland & waybar

--- home-manager/shared/waybar.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  programs.waybar = {
-    enable = true;
-  };
-
-  home.file.".config/waybar/config.jsonc".source = ./waybar/config.jsonc;
-  home.file.".config/waybar/style.css".source = ./waybar/style.css;
-}
