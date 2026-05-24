361a670cb659056d8c9913f9691611c64012ff5e
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-24 18:18:58 +0200

    fix: adjusted hardware for steamdeck

--- hosts/steamdeck/boot.nix
+++ hosts/steamdeck/boot.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, modulesPath, ... }: {
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
