c350f32d1806af894d3832a058ebf63b5977df7b
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-05-24 18:18:52 +0200

    fix: adjusted hardware for steamdeck

--- hosts/steamdeck/hardware.nix
+++ hosts/steamdeck/hardware.nix
@@ -1,25 +1,29 @@
 { config, lib, pkgs, modulesPath, ... }: {
   # Disk config
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/fad2025e-e33e-45a5-af9a-8338cca5ee97";
+    fileSystems."/" =
+    { device = "/dev/disk/by-uuid/be9cbef6-0a9f-4a94-8627-766ea4cff995";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2AAA-B9A7";
+    { device = "/dev/disk/by-uuid/F28F-32D3";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [{ 
-    device = "/dev/disk/by-uuid/dde0c8d0-6d7c-4cb5-ac79-5075540c8fe1"; 
-  }];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/bf363541-aef5-42b5-ac4b-316a76b262e0"; }
+    ];
 
   # Steam Hardware
   hardware.steam-hardware.enable = true;
 
   # ZRAM
   zramSwap.enable = true;
+
+  # Microcode
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
