38f7410432d4e7947ba4ba961274cb5f761f8410
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-16 01:51:42 +0200

    Trying another way

--- users/sakulflee/git.nix
+++ users/sakulflee/git.nix
@@ -5,10 +5,8 @@
       user.name = "@SakulFlee | Lukas Weber";
       user.email = "dev@sakul-flee.de";
 
-      gpg.program = "${pkgs.gnupg}/bin/gpg";
-      signing.key = "0A96C9AA72DB019DE171E7F77F0C6AF1F56A9E05";
-      signing.signByDefault = true;
-      signing.format = "openpgp";
+      signingKey = "0A96C9AA72DB019DE171E7F77F0C6AF1F56A9E05";
+      signByDefault = true;
     };
   };
 }
