b3e80c37b9132375b20c5394135c4317f3a00b93
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-16 00:01:02 +0200

    Trying a better way

--- users/sakulflee/gpg.nix
+++ users/sakulflee/gpg.nix
@@ -1,7 +1,20 @@
-{ config, pkgs, lib, inputs, ...}: {
+{ config, osConfig, pkgs, lib, inputs, ...}: {
   imports = [
     inputs.sops-nix.homeManagerModules.sops
   ];
+
+  sops = {
+    # Take values from Nix* sops config
+    defaultSopsFile   = osConfig.sops.defaultSopsFile;
+    defaultSopsFormat = osConfig.sops.defaultSopsFormat;
+    age.sshKeyPaths   = osConfig.sops.age.sshKeyPaths;
+
+    # Declare secret to expose here
+    secrets."gpg-private-key" = {};
+  };
+
+  # Store secret GPG key into gpg homedir
+  sops.secrets."gpg-private-key".path = "${config.programs.gpg.homedir}/private-key.asc";
 
   programs.gpg = {
     enable = true;
@@ -15,15 +28,5 @@
     enable = true;
     pinentryPackage = pkgs.pinentry-qt; # KDE popup for password entry
     enableSshSupport = true;            # Optional: lets GPG act as your SSH agent too
-  };
-
-  home.activation = {
-    importGpgKey = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      # Only import if GPG doesn't already have this key active
-      if ! ${pkgs.gnupg}/bin/gpg --list-secret-keys 0A96C9AA72DB019DE171E7F77F0C6AF1F56A9E05 >/dev/null 2>&1; then
-        echo "Importing private GPG key from SOPS..."
-        ${pkgs.gnupg}/bin/gpg --import /run/secrets/gpg-private-key
-      fi
-    '';
   };
 }
