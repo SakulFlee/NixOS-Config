bae1da8d4d425ea587c18cfbc8269d04c142f85f
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-15 19:29:33 +0200

    Trying to define sops-nix

--- flake.nix
+++ flake.nix
@@ -2,6 +2,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
