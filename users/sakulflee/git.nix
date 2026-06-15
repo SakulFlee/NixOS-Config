47219d8821e228e091cb56fd55185b5f1658713c
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-16 01:48:19 +0200

    Trying without nesting

--- users/sakulflee/git.nix
+++ users/sakulflee/git.nix
@@ -6,10 +6,9 @@
       user.email = "dev@sakul-flee.de";
 
       gpg.program = "${pkgs.gnupg}/bin/gpg";
-      signing = {
-        key = "0A96C9AA72DB019DE171E7F77F0C6AF1F56A9E05";
-        signByDefault = true;
-      };
+      signing.key = "0A96C9AA72DB019DE171E7F77F0C6AF1F56A9E05";
+      signing.signByDefault = true;
+      signing.format = "openpgp";
     };
   };
 }
