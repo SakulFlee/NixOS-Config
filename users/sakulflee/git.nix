5d9e882ec3cb06c5714962a275f57aacdeb7e355
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-16 01:54:26 +0200

    Nesting.

--- users/sakulflee/git.nix
+++ users/sakulflee/git.nix
@@ -2,9 +2,11 @@
   programs.git = {
     enable = true;
     settings = {
-      user.name = "@SakulFlee | Lukas Weber";
-      user.email = "dev@sakul-flee.de";
-      user.signingKey = "0A96C9AA72DB019DE171E7F77F0C6AF1F56A9E05";
+      user = {
+        name = "@SakulFlee | Lukas Weber";
+        email = "dev@sakul-flee.de";
+        signingKey = "0A96C9AA72DB019DE171E7F77F0C6AF1F56A9E05";
+      };
       signing.signByDefault = true;
     };
   };
