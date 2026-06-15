a03d164aa2fd37e00ee1c3f08f0c10e6bb171080
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-15 22:54:17 +0200

    Added Plasma config

--- shared/kde/plasma-manager.nix
+++ shared/kde/plasma-manager.nix
@@ -5,23 +5,66 @@
 
   programs.plasma = {
     enable = true;
+
+# Desktop Workspace Settings
     workspace = {
       clickItemTo = "open";
       lookAndFeel = "org.kde.breezedark.desktop";
-      wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Next/contents/images/5120x2880.png";
+      wallpaperPlugin = "org.kde.image";
+      wallpaper = "/usr/share/wallpapers/cachyos-wallpapers/PurpleFeathers.png";
+    };
+
+    # Custom Folder View for your Synced Desktop Folder
+    desktop = {
+      widgets = [
+        {
+          name = "org.kde.plasma.folder";
+          config = {
+            General = {
+              url = "file:///home/sakulflee/Sync/Desktop";
+              sortMode = "2";    # Sort by Date
+              alignment = "1";   # Icons on the Right
+              arrangement = "1"; # Organize in Rows
+            };
+          };
+        }
+      ];
     };
 
     panels = [
-      # Configure your main taskbar panel declaratively!
+      # Top Panel: App menu, system tray, launcher, etc.
+      {
+        location = "top";
+        height = 44;
+        widgets = [
+          "org.kde.plasma.kickoff"
+          "org.kde.plasma.appmenu"
+          "org.kde.plasma.panelspacer"
+          "org.kde.plasma.pager"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.digitalclock"
+        ];
+      }
+
+      # Bottom Panel: Dock
       {
         location = "bottom";
         height = 44;
         widgets = [
-          "org.kde.plasma.kickoff"
-          "org.kde.plasma.pager"
-          "org.kde.plasma.icontasks"
-          "org.kde.plasma.systemtray"
-          "org.kde.plasma.digitalclock"
+          {
+            name = "org.kde.plasma.icontasks";
+            config = {
+              General = {
+                launchers = [
+                  "preferred://filemanager"
+                  "preferred://browser"
+                  "applications:discord.desktop"
+                  "applications:brave-browser.desktop"
+                  "applications:obsidian.desktop"
+                ];
+              };
+            };
+          }
         ];
       }
     ];
