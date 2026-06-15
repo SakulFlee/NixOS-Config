9d7aed49de42f9df2227d66acc67d419f2f71cf4
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-15 19:22:33 +0200

    Removed Sunshine PR

--- flake.nix
+++ flake.nix
@@ -2,14 +2,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-sunshine-pr.url = "github:NixOS/nixpkgs/pull/521906/head";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, nixpkgs-sunshine-pr, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs: {
     nixosConfigurations.Cindry = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { 
@@ -19,17 +18,13 @@
           system = "x86_64-linux"; 
           config = { allowUnfree = true; }; 
         }; 
-        sunshine-pr = import nixpkgs-sunshine-pr.outPath { 
-          system = "x86_64-linux"; 
-        }; 
       };
       modules = [ 
         ./hosts/cindry/configuration.nix 
-        ({ unstable, sunshine-pr, ... }: {
+        ({ unstable, ... }: {
           nixpkgs.overlays = [
             (final: prev: {
               lmstudio = unstable.lmstudio;
-              sunshine = sunshine-pr.sunshine;
             })
           ];
         })
@@ -45,17 +40,13 @@
           system = "x86_64-linux"; 
           config = { allowUnfree = true; }; 
         }; 
-        sunshine-pr = import nixpkgs-sunshine-pr.outPath { 
-          system = "x86_64-linux"; 
-        }; 
       };
       modules = [ 
         ./hosts/steamdeck/configuration.nix 
-        ({ unstable, sunshine-pr, ... }: {
+        ({ unstable, ... }: {
           nixpkgs.overlays = [
             (final: prev: {
               lmstudio = unstable.lmstudio;
-              sunshine = sunshine-pr.sunshine;
             })
           ];
         })
