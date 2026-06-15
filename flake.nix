8e99cae4dbc33549099c6df0757f1a8666696b6a
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-15 22:37:21 +0200

    Missing bracket?

--- flake.nix
+++ flake.nix
@@ -41,22 +41,23 @@
       };
     in {
       nixosConfigurations = {
-      Cindry = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = sharedArgs;
-        modules = [ 
-          ./hosts/cindry/configuration.nix 
-          sharedOverlayModule
-        ];
-      };
+        Cindry = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = sharedArgs;
+          modules = [ 
+            ./hosts/cindry/configuration.nix 
+            sharedOverlayModule
+          ];
+        };
 
-      SteamDeck = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = sharedArgs;
-        modules = [ 
-          ./hosts/steamdeck/configuration.nix 
-          sharedOverlayModule
-        ];
+        SteamDeck = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = sharedArgs;
+          modules = [ 
+            ./hosts/steamdeck/configuration.nix 
+            sharedOverlayModule
+          ];
+        };
       };
     };
   };
