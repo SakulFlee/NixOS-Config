586248f0aeac96c560406eb89a15ec9a9020b88e
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-16 01:52:15 +0200

    Corrected signByDefault key

--- users/sakulflee/git.nix
+++ users/sakulflee/git.nix
@@ -6,7 +6,7 @@
       user.email = "dev@sakul-flee.de";
 
       signingKey = "0A96C9AA72DB019DE171E7F77F0C6AF1F56A9E05";
-      signByDefault = true;
+      signing.signByDefault = true;
     };
   };
 }
