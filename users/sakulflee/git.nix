963989cc75f8e7ed6d705aa380201b647f3a67f4
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-16 01:52:56 +0200

    Trying with user. domain?

--- users/sakulflee/git.nix
+++ users/sakulflee/git.nix
@@ -4,8 +4,7 @@
     settings = {
       user.name = "@SakulFlee | Lukas Weber";
       user.email = "dev@sakul-flee.de";
-
-      signingKey = "0A96C9AA72DB019DE171E7F77F0C6AF1F56A9E05";
+      user.signingKey = "0A96C9AA72DB019DE171E7F77F0C6AF1F56A9E05";
       signing.signByDefault = true;
     };
   };
