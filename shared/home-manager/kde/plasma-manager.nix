2bb6f9b2f584e04afc3e84ff4180e95854455cbc
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-16 04:10:49 +0200

    Trying to move folder

--- shared/home-manager/kde/plasma-manager.nix
+++ shared/home-manager/kde/plasma-manager.nix
@@ -17,12 +17,12 @@
         {
           name = "org.kde.plasma.folder";
           position = {
-            horizontal = 100;
-            vertical = 100;
+            horizontal = 400;
+            vertical = 200;
           };
           size = {
-            width = 20;
-            height = 20;
+            width = 100;
+            height = 100;
           };
           config = {
             General = {
