37ba753152439ae4edd8f4fa148fbcb268ce7409
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-16 20:32:54 +0200

    Adding env-vars to systemd globally

--- shared/nix/kde/_.nix
+++ shared/nix/kde/_.nix
@@ -1,5 +1,6 @@
 { ... }: {
   imports = [
+    ./envs.nix
     ./plasma.nix
     ./xdg-portal.nix
   ];
---
+++ shared/nix/kde/envs.nix
@@ -0,0 +1,5 @@
+{ ... }: {
+  services.xserver.displayManager.sessionCommands = ''
+    ${pkgs.systemd}/bin/systemctl --user import-environment DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY WAYLAND_DISPLAY
+  '';
+}
