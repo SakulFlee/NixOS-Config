76737be6d6dab8dfc202736001e8830c8d66e8fe
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-16 20:22:17 +0200

    Added LTS and gaming focused kernel

--- shared/nix/kernel.nix
+++ shared/nix/kernel.nix
@@ -1,4 +1,17 @@
 { pkgs, ... }: {
-  # Kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # ZEN Kernel for gaming
+  boot.kernelPackages = pkgs.linuxPackages_latest_zen;
+  boot.loader.systemd-boot.description = "NixOS (Gaming/ZEN)";
+
+  specialisation = {
+    lts.configuration = {
+      system.nixos.tags = [ "LTS" ];
+      boot.kernelPackages = pkgs.linuxPackages; # Defaults to latest LTS
+    };
+
+    gaming.configuration = {
+      system.nixos.tags = [ "Latest" ];
+      boot.kernelPackages = pkgs.linuxPackages_latest;
+    };
+  };
 }
