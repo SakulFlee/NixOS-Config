16a4aed0f23991c58f2b9ebf187f380404d6efa2
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-16 20:07:04 +0200

    Temporarily removed previous file sync service

--- shared/nix/_.nix
+++ shared/nix/_.nix
@@ -15,7 +15,7 @@
     ./nixpkgs-unfree.nix        
     ./printing.nix             
     ./rclone-sync.nix
-    ./service-file-sync.nix
+    # ./service-file-sync.nix
     ./service-nixos-config-rebuilder.nix
     ./sops.nix                  
     ./ssh.nix                   
