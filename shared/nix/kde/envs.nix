ab96fe959e41b69b1e77ca5756694fd41d199a50
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-16 20:33:11 +0200

    Missing pkgs

--- shared/nix/kde/envs.nix
+++ shared/nix/kde/envs.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.xserver.displayManager.sessionCommands = ''
     ${pkgs.systemd}/bin/systemctl --user import-environment DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY WAYLAND_DISPLAY
   '';
