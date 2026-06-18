8afcf9991b8a4beb062d14598cb721a47c67ac19
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 15:25:20 +0200

    Fixed amdgpu config

--- shared/nix-hardware/gpu-amdgpu.nix
+++ shared/nix-hardware/gpu-amdgpu.nix
@@ -9,22 +9,5 @@
 
   hardware.graphics = {
     enable = true;
-    extraPackages = [ pkgs.mesa ];
-  };
-
-  hardware.nvidia = {
-    # Modesetting
-    modesetting.enable = true;
-
-    # Experimental!
-    powerManagement.enable = true;
-    powerManagement.finegrained = true;
-
-    # Driver version
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    open = true;
-
-    # Include nvidia settings app
-    nvidiaSettings = true;
   };
 }
