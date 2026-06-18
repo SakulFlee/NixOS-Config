02a7a3da8576220ba2e782a794e25150c3069c65
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 18:31:26 +0200

    Trying to set commit.gpgsign in git instead

--- users/sakulflee/git.nix
+++ users/sakulflee/git.nix
@@ -7,7 +7,7 @@
         email = "dev@sakul-flee.de";
         signingKey = "0A96C9AA72DB019DE171E7F77F0C6AF1F56A9E05";
       };
-      signing.signByDefault = true;
+      commit.gpgsign = true;
     };
   };
 }
