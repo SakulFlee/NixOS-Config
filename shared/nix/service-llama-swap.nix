b8e76ef6905df631213f71cc5d712023e448a89e
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 20:20:55 +0200

    Added gemma4:e4b

--- shared/nix/service-llama-swap.nix
+++ shared/nix/service-llama-swap.nix
@@ -8,9 +8,14 @@ in
     enable = true;
     settings = {
       models = {
-        "qwen35-9b" = {
+        "qwen3.5-9b-mtp" = {
           cmd = ''
             ${llama-server} --port ''${PORT} -hf unsloth/Qwen3.5-9B-MTP-GGUF
+          '';
+        };
+        "gemma-4-e4b-it-qat" = {
+          cmd = ''
+            ${llama-server} --port ''${PORT} -hf unsloth/gemma-4-E4B-it-qat-GGUF
           '';
         };
       };
