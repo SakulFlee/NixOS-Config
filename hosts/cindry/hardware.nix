7d5fd163837e9538e747493530978367c961d2cb
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 15:19:46 +0200

    Moved redistributable hardware into shared hardware

--- hosts/cindry/hardware.nix
+++ hosts/cindry/hardware.nix
@@ -3,6 +3,7 @@
     ../../shared/nix-hardware/gpu-nvidia-prime.nix
     ../../shared/nix-hardware/gpu-amdgpu.nix
     ../../shared/nix-hardware/boot-loader.nix
+    ../../shared/nix-hardware/redistributable-hardware.nix
   ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
@@ -38,7 +39,6 @@
     [ { device = "/dev/disk/by-uuid/417a545e-a9a0-472b-adc1-2428812c1110"; }
     ];
 
-  hardware.enableRedistributableFirmware = true;
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   # NVIDIA PRIME
