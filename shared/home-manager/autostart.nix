f6bd397ed926f51f760b0706e23255d124c5b72d
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 17:33:10 +0200

    Corrected autostart entries

--- shared/home-manager/autostart.nix
+++ shared/home-manager/autostart.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }: {
   # Link autostart entries
-  xdg.configFile."autostart/deskflow.desktop".source = "${pkgs.steam}/share/applications/deskflow.desktop";
-  xdg.configFile."autostart/sunshine.desktop".source = "${pkgs.steam}/share/applications/sunshine.desktop";
-  xdg.configFile."autostart/rustdesk.desktop".source = "${pkgs.steam}/share/applications/rustdesk.desktop";
+  xdg.configFile."autostart/deskflow.desktop".source = "${pkgs.deskflow}/share/applications/deskflow.desktop";
+  xdg.configFile."autostart/sunshine.desktop".source = "${pkgs.sunshine}/share/applications/sunshine.desktop";
+  xdg.configFile."autostart/rustdesk.desktop".source = "${pkgs.rustdesk}/share/applications/rustdesk.desktop";
   xdg.configFile."autostart/discord.desktop".source = "${pkgs.discord}/share/applications/discord.desktop";
   xdg.configFile."autostart/obsidian.desktop".source = "${pkgs.obsidian}/share/applications/obsidian.desktop";
   xdg.configFile."autostart/steam.desktop".source = "${pkgs.steam}/share/applications/steam.desktop";
