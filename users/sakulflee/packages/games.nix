54db230f7db57b457967a4a3633c105af0d97a80
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 18:30:00 +0200

    Removed all but protonplus

--- users/sakulflee/packages/games.nix
+++ users/sakulflee/packages/games.nix
@@ -1,22 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    # Proton managers
+    # Proton manager
     protonplus
-    protonup-ng
-    protonup-qt
-    protonup-rs
-
-    # DW Proton for Gacha games
-    (runCommand "dwproton-bin-fixed" {} ''
-      mkdir -p $out/bin
-      ln -s ${dwproton-bin} $out/bin/dwproton
-    '')
-
-    # GE Proton for other games
-    (runCommand "proton-ge-bin-fixed" {} ''
-      mkdir -p $out/bin
-      ln -s ${proton-ge-bin} $out/bin/proton-ge
-    '')
   ];
 }
 
