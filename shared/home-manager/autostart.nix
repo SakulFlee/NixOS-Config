073513eb81b43c42934237815f9c5be165c946b9
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 17:13:05 +0200

    Added sunshine to autostart

--- shared/home-manager/autostart.nix
+++ shared/home-manager/autostart.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   # Link autostart entries
   xdg.configFile."autostart/deskflow.desktop".source = "${pkgs.steam}/share/applications/deskflow.desktop";
+  xdg.configFile."autostart/sunshine.desktop".source = "${pkgs.steam}/share/applications/sunshine.desktop";
   xdg.configFile."autostart/discord.desktop".source = "${pkgs.discord}/share/applications/discord.desktop";
   xdg.configFile."autostart/obsidian.desktop".source = "${pkgs.obsidian}/share/applications/obsidian.desktop";
   xdg.configFile."autostart/steam.desktop".source = "${pkgs.steam}/share/applications/steam.desktop";
