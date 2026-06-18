41beedfad5bc88a7d4e28293535c98c3dea5e089
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 14:59:56 +0200

    Cleaned up audio configuration

--- shared/nix/audio.nix
+++ shared/nix/audio.nix
@@ -1,12 +1,22 @@
 { pkgs, ...}:
 {
-  # Audio
+  # Disable PulseAudio (outdated)
   services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
+
+  # Enable PipeWire (replaces PulseAudio)
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  # Enabled "RealtimeKit System Service"
+  # Required for PulseAudio and/or PipeWire to work correctly.
+  security.rtkit.enable = true;
+
+  # Additional packages to control PipeWire better
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+  ];
 }
