9bb0fd6a523ca53b76b944a3483a0b7d4cda1cb6
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 15:48:54 +0200

    Corrected kernel.nix

--- shared/nix/kernel.nix
+++ shared/nix/kernel.nix
@@ -1,21 +1,16 @@
 { lib, pkgs, ... }: {
-  # Default kernel
+  # Default kernel is gaming focused (ZEN)
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
   specialisation = {
-    lts.configuration = {
-      system.nixos.tags = [ "Gaming" ];
-      boot.kernelPackages = lib.mkForce pkgs.linuxPackages_zen; # ZEN for gaming
+    latest.configuration = {
+      system.nixos.tags = [ "Latest" ];
+      boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
     };
 
     lts.configuration = {
       system.nixos.tags = [ "LTS" ];
       boot.kernelPackages = lib.mkForce pkgs.linuxPackages; # Defaults to latest LTS
-    };
-
-    gaming.configuration = {
-      system.nixos.tags = [ "Latest" ];
-      boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
     };
   };
 }
