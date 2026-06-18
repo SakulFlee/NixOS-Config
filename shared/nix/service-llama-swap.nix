adab5ce87a5c12ff48658fd803150571c076d9a3
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 19:33:21 +0200

    Trying better name

--- shared/nix/service-llama-swap.nix
+++ shared/nix/service-llama-swap.nix
@@ -8,7 +8,7 @@ in
     enable = true;
     settings = {
       models = {
-        name = "qwen3.5 9B";
+        name = "qwen3.5-9B";
         cmd = ''
           ${llama-server} --port ''${PORT} -hf unsloth/Qwen3.5-9B-MTP-GGUF
         '';
