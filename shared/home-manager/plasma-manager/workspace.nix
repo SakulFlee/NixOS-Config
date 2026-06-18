f011ad31fcd9ba30da04ccb4350ee8c96c998d3b
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 23:02:35 +0200

    KDE: No active corners

--- shared/home-manager/plasma-manager/workspace.nix
+++ shared/home-manager/plasma-manager/workspace.nix
@@ -5,6 +5,15 @@
     wallpaper = "/usr/share/wallpapers/cachyos-wallpapers/PurpleFeathers.png";
   };
 
+  programs.plasma.configFile."kwinrc" = {
+    "ElectricBorders" = {
+      "BottomLeft" = "NoAction";
+      "BottomRight" = "NoAction";
+      "TopLeft" = "NoAction";
+      "TopRight" = "NoAction";
+    };
+  };
+
   programs.plasma.startup.desktopScript."set_desktop_url" = {
     text = ''
       var allDesktops = desktops();
