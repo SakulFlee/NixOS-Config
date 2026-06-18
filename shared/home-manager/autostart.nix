f76ff72f1c419d4a2fa9cdfb0897249431a00f42
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-18 17:13:25 +0200

    Added rustdesk to autostart

--- shared/home-manager/autostart.nix
+++ shared/home-manager/autostart.nix
@@ -2,6 +2,7 @@
   # Link autostart entries
   xdg.configFile."autostart/deskflow.desktop".source = "${pkgs.steam}/share/applications/deskflow.desktop";
   xdg.configFile."autostart/sunshine.desktop".source = "${pkgs.steam}/share/applications/sunshine.desktop";
+  xdg.configFile."autostart/rustdesk.desktop".source = "${pkgs.steam}/share/applications/rustdesk.desktop";
   xdg.configFile."autostart/discord.desktop".source = "${pkgs.discord}/share/applications/discord.desktop";
   xdg.configFile."autostart/obsidian.desktop".source = "${pkgs.obsidian}/share/applications/obsidian.desktop";
   xdg.configFile."autostart/steam.desktop".source = "${pkgs.steam}/share/applications/steam.desktop";
