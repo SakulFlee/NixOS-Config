b43835a378b1976a201a3453963cc7f5c2173e1c
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-19 02:37:09 +0200

    Minimizing steam by default

--- shared/home-manager/autostart.nix
+++ shared/home-manager/autostart.nix
@@ -5,7 +5,17 @@
   xdg.configFile."autostart/rustdesk.desktop".source = "${pkgs.rustdesk}/share/applications/rustdesk.desktop";
   xdg.configFile."autostart/discord.desktop".source = "${pkgs.discord}/share/applications/discord.desktop";
   xdg.configFile."autostart/obsidian.desktop".source = "${pkgs.obsidian}/share/applications/obsidian.desktop";
-  xdg.configFile."autostart/steam.desktop".source = "${pkgs.steam}/share/applications/steam.desktop";
   xdg.configFile."autostart/keepassxc.desktop".source = "${pkgs.keepassxc}/share/applications/keepassxc.desktop";
+
+  xdg.configFile."autostart/steam.desktop".text = ''
+    [Desktop Entry]
+    Name=Steam
+    Comment=Application for managing and playing games on Steam
+    Exec=${pkgs.steam}/bin/steam -silent
+    Icon=steam
+    Terminal=false
+    Type=Application
+    Categories=Network;FileTransfer;Game;
+  '';
 }
 
