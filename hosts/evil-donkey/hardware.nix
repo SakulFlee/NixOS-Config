49dbcdb223c65af245e9541f022ed9cda1f7a727
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-19 20:59:38 +0200

    Added Evil-Donkey hardware config

--- hosts/evil-donkey/hardware.nix
+++ hosts/evil-donkey/hardware.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, modulesPath, ... }: {
   imports = [
-    ../../shared/nix-hardware/gpu-nvidia-prime.nix
+    ../../shared/nix-hardware/gpu-nvidia.nix
     ../../shared/nix-hardware/gpu-amdgpu.nix
     ../../shared/nix-hardware/redistributable-hardware.nix
     ../../shared/nix-hardware/microcode.nix
@@ -11,11 +11,40 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
   # Additional boot arguments should _mostly_ stay in the host config as they tend to highly customized between hosts
-  # TODO
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
 
   # Disk config
-  # TODO 
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/20bdfb75-9bbc-4d5b-8b18-c20dfb2bbc22";
+      fsType = "btrfs";
+    };
 
-  # NVIDIA PRIME
-  # TODO 
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/20bdfb75-9bbc-4d5b-8b18-c20dfb2bbc22";
+      fsType = "btrfs";
+      options = [ "subvol=home" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/20bdfb75-9bbc-4d5b-8b18-c20dfb2bbc22";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/CD0F-328B";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+  fileSystems."/mnt/ssd" =
+    { device = "/dev/disk/by-uuid/02c8c213-7a5b-4bf1-97da-749dd87b3417";
+      fsType = "btrfs";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/66f414be-c83d-4b98-aa49-ca21bd57eb00"; }
+    ];
 }
