d31db9b1c51910cbb6960d7da3080c644c7bc599
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-20 00:26:43 +0200

    Added & enabled akonadi service

--- shared/nix/_.nix
+++ shared/nix/_.nix
@@ -2,6 +2,7 @@
   imports = [
     ../gc.nix
     ./kde/_.nix 
+    ./akonadi.nix
     ./audio.nix                 
     ./avahi.nix
     ./bluetooth.nix
---
+++ shared/nix/akonadi.nix
@@ -0,0 +1,10 @@
+{ ...}:
+{
+  services.akonadi = {
+    enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    libsForQt5.kdepim-runtime
+  ];
+}
