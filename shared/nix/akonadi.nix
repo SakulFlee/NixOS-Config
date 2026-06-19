48fdf619fa070866c9fb2af1bf195f41efc9a456
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-20 00:36:12 +0200

    Fixing Akonadi

--- shared/nix/akonadi.nix
+++ shared/nix/akonadi.nix
@@ -5,6 +5,14 @@
   };
 
   environment.systemPackages = with pkgs; [
-    libsForQt5.kdepim-runtime
+    kdePackages.kdepim-runtime
+    kdePackages.akonadi
+    kdePackages.akonadi-mime
+    kdePackages.akonadi-search
+    kdePackages.akonadi-calendar
+    kdePackages.akonadi-calendar-tools
+    kdePackages.akonadi-contacts
+    kdePackages.akonadi-import-wizard
+    kdePackages.mbox-importer
   ];
 }
