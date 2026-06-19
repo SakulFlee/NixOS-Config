c85eb0527d2fb8b02e9a205a709b6d47d1d21f76
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-19 02:42:05 +0200

    Removed Deskflow from starting automatically

--- shared/home-manager/autostart.nix
+++ shared/home-manager/autostart.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }: {
   # Link autostart entries
-  xdg.configFile."autostart/deskflow.desktop".source = "${pkgs.deskflow}/share/applications/org.deskflow.deskflow.desktop";
   xdg.configFile."autostart/sunshine.desktop".source = "${pkgs.sunshine}/share/applications/sunshine.desktop";
   xdg.configFile."autostart/rustdesk.desktop".source = "${pkgs.rustdesk}/share/applications/rustdesk.desktop";
   xdg.configFile."autostart/obsidian.desktop".source = "${pkgs.obsidian}/share/applications/obsidian.desktop";
