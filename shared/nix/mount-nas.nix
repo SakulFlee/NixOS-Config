fe3243e949f83e47a3513930fb2427ef3fc94eff
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-21 19:25:01 +0200

    Updated NAS mounts

--- shared/nix/mount-nas.nix
+++ shared/nix/mount-nas.nix
@@ -1,24 +1,43 @@
-{ config, pkgs, inputs, ... }: {
+{ config, pkgs, inputs, ... }: 
+let 
+  shares = [
+    "personal_folder"
+    "Doujin"
+    "Hentai"
+    "Movies"
+    "Music"
+    "NSFW"
+    "PBS"
+    "qBittorrent"
+    "Shows"
+  ];
+
+  nasServer = "//192.168.178.250";
+
+  makeMount = shareName: {
+    name = "/mnt/nas/${shareName}";
+    value = {
+      device = "${nasServer}/${shareName}";
+      fsType = "cifs";
+      options = [
+      # Crucial: Link to the decrypted runtime path provided by sops-nix
+        "credentials=/run/secrets/smb_credentials"
+        
+        "uid=1000"        # Maps files to your local user ID
+        "gid=100"         # Maps files to the 'users' group ID
+        "file_mode=0664"  # Gives you read/write access
+        "dir_mode=0775"   # Gives you read/write/execute on folders
+      
+        # Automount on-demand so booting doesn't hang if the NAS is asleep
+        "noauto"
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=60" # Unmounts after inactivity
+      ];
+    };
+  };
+{
   # Install the SMB/CIFS client utilities
   environment.systemPackages = [ pkgs.cifs-utils ];
 
-  # Define the actual SMB Mount
-  fileSystems."/mnt/nas" = {
-    device = "//192.168.178.250/personal_folder";
-    fsType = "cifs";
-    options = [
-      # Crucial: Link to the decrypted runtime path provided by sops-nix
-      "credentials=/run/secrets/smb_credentials"
-      
-      "uid=1000"        # Maps files to your local user ID
-      "gid=100"         # Maps files to the 'users' group ID
-      "file_mode=0664"  # Gives you read/write access
-      "dir_mode=0775"   # Gives you read/write/execute on folders
-      
-      # Automount on-demand so booting doesn't hang if the NAS is asleep
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=60" # Unmounts after 60s of inactivity to save network resources
-    ];
-  };
+  fileSystems = builtins.listToAttrs (map makeMount shares);
 }
