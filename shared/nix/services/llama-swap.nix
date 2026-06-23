82563947b9ff360625ea7c728ded556939edc6a5
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-06-23 18:10:56 +0200

    llama-swap: Optimizing config

--- shared/nix/services/llama-swap.nix
+++ shared/nix/services/llama-swap.nix
@@ -15,46 +15,45 @@ in
           ${llama-server} \
             --host 127.0.0.1 \
             --port ''${PORT} \
+            -fa on \
             -c 65536
         '';
         "with_fit" = ''
-          ''${default} \
-            -fit on
+          -fit on
         '';
         "with_mtp" = ''
-          ''${default} \
-            --spec-type draft-mtp
-        '';
-        "with_mtp_and_fit" = ''
-          ''${default} \
-            -fit on \
-            --spec-type draft-mtp
+          --spec-type draft-mtp \
+          --spec-draft-n-max 4
         '';
       };
       models = {
         "[unsloth] Gemma4 26B-A4B @Q4_K_XL - IT QAT MTP" = {
           cmd = ''
-            ''${with_mtp_and_fit} \
-              -hf unsloth/gemma-4-26B-A4B-it-qat-GGUF:UD-Q4_K_XL \
-              --spec-draft-n-max 4 \
-              -fa on
+            ''${default} \
+            ''${with_mtp} \
+            ''${with_fit} \
+              -hf unsloth/gemma-4-26B-A4B-it-qat-GGUF:UD-Q4_K_XL 
           '';
         };
         "[MTP] empero-ai/Qwythos-9B-Claude-Mythos-5-1M-GGUF:Q4_K_M" = {
           cmd = ''
-            ''${with_mtp_and_fit} \
+            ''${default} \
+            ''${with_mtp} \
+            ''${with_fit} \
               -hf empero-ai/Qwythos-9B-Claude-Mythos-5-1M-GGUF:Q4_K_M \
               -m Qwythos-9B-Claude-Mythos-5-1M-MTP-Q4_K_M.gguf
           '';
         };
         "unsloth/Qwen3.5-9B-MTP-GGUF" = {
           cmd = ''
+            ''${default} \
             ''${with_mtp} \
               -hf unsloth/Qwen3.5-9B-MTP-GGUF
           '';
         };
         "unsloth/gemma-4-E4B-it-qat-GGUF:UD-Q4_K_XL (Laptop config)" = {
           cmd = ''
+            ''${default} \
             ''${with_mtp} \
               -hf unsloth/gemma-4-E4B-it-qat-GGUF:UD-Q4_K_XL \
               -c 8192 \
@@ -63,24 +62,31 @@ in
         };
         "unsloth/gemma-4-E4B-it-qat-GGUF:UD-Q4_K_XL" = {
           cmd = ''
-            ''${with_mtp_and_fit} \
+            ''${default} \
+            ''${with_mtp} \
+            ''${with_fit} \
               -hf unsloth/gemma-4-E4B-it-qat-GGUF:UD-Q4_K_XL
           '';
         };
         "unsloth/gemma-4-12B-it-qat-GGUF:UD-Q4_K_XL" = {
           cmd = ''
-            ''${with_mtp_and_fit} \
+            ''${default} \
+            ''${with_mtp} \
+            ''${with_fit} \
               -hf unsloth/gemma-4-12B-it-qat-GGUF:UD-Q4_K_XL
           '';
         };
         "unsloth/gemma-4-26B-A4B-it-qat-GGUF:UD-Q4_K_XL" = {
           cmd = ''
-            ''${with_mtp_and_fit} \
+            ''${default} \
+            ''${with_mtp} \
+            ''${with_fit} \
               -hf unsloth/gemma-4-26B-A4B-it-qat-GGUF:UD-Q4_K_XL
           '';
         };
         "yuxinlu1/gemma-4-12B-agentic-fable5-composer2.5-v2-3.5x-tau2-GGUF" = {
           cmd = ''
+            ''${default} \
             ''${with_fit} \
               -hf yuxinlu1/gemma-4-12B-agentic-fable5-composer2.5-v2-3.5x-tau2-GGUF:Q4_K_M
           '';
