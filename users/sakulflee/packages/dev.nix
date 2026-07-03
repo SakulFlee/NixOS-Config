f85794ff7dcb7ef2916c332b007479123ecc66de
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-03 18:43:02 +0200

    vsc: trying without profile block

--- users/sakulflee/packages/dev.nix
+++ users/sakulflee/packages/dev.nix
@@ -9,18 +9,17 @@
 
   programs.vscodium = {
     enable = true;
-    profiles.main = {
-      userSettings = {
-        "telemetry.telemetryLevel" = "off";
-        "update.mode" = "none";
-      };
-
-      extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix         # Nix syntax highlighting
-        jnoortheen.nix-ide   # Nix language server support
-        vscodevim.vim        # Vim emulation
-      ];
+    
+    userSettings = {
+      "telemetry.telemetryLevel" = "off";
+      "update.mode" = "none";
     };
+
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix         # Nix syntax highlighting
+      jnoortheen.nix-ide   # Nix language server support
+      vscodevim.vim        # Vim emulation
+    ];
   };
 }
 
