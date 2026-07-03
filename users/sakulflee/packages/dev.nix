e35a19b19bab0ed5d7ab29e6de8e942adb42949b
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-03 19:00:42 +0200

    vsc: trying to add rust-test-adapter

--- users/sakulflee/packages/dev.nix
+++ users/sakulflee/packages/dev.nix
@@ -23,7 +23,13 @@
         # Rust
         rust-lang.rust-analyzer
         tamasfe.even-better-toml
-        Swellaby.vscode-rust-test-adapter
+        (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+          mktid = "swellaby.vscode-rust-test-adapter";
+          name = "vscode-rust-test-adapter";
+          publisher = "swellaby";
+          version = "0.11.1";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        })
 
         # Nix
         bbenoist.nix
