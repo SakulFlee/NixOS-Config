3b2061c0684c97ba01b049adc7b8115794f087ed
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-04 03:22:29 +0200

    Added Jdownloader2
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- shared/nix/services/flatpak.nix
+++ shared/nix/services/flatpak.nix
@@ -1,3 +1,18 @@
 { ... }: {
     services.flatpak.enable = true;
+
+    # Auto updates
+    services.flatpak.update.auto.enable = true;
+    services.flatpak.update.auto.onCalendar = "weekly";
+
+    services.flatpak.packages = [
+        "org.jdownloader.JDownloader"
+    ];
+
+    services.flatpak.remotes = [
+        {
+          name = "flathub";
+          location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+        }
+    ];
 }
