2a79e89cb3d4b2b8108547dfd230cfa24782df90
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-05 20:21:18 +0200

    wireguard: trying with wg-quick instead for DNS
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- shared/nix/wireguard.nix
+++ shared/nix/wireguard.nix
@@ -2,11 +2,11 @@
 let
   hostCfg = {
     "Evil-Donkey" = {
-      ips = [ "10.100.0.2/32" ];
+      address = [ "10.100.0.2/32" ];
       publicKey = "tDj18GzgpKzFsfu0JdltiienO5uvJDw1e4Hwq1551DI=";
     };
     "Cindry" = {
-      ips = [ "10.100.0.3/32" ];
+      address = [ "10.100.0.3/32" ];
       publicKey = "ClMVYG1CLPUe4O8gWKbriZYe46vzHm5jV0vL8sCJsDI=";
     };
   };
@@ -16,18 +16,15 @@ in {
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.secrets."wireguard_admin_private_key" = { };
 
-  networking.wireguard.interfaces.wg0 = {
-    ips = this.ips;
+  networking.wg-quick.interfaces.wg0 = {
+    address = this.address;
     privateKeyFile = config.sops.secrets.wireguard_admin_private_key.path;
+    dns = [ "10.0.0.116" ];
     peers = [{
       publicKey = this.publicKey;
       endpoint = "vpn.sakul-flee.de:51820";
       allowedIPs = [ "10.0.0.0/24" "192.168.178.0/24" "fdbe::/64" ];
       persistentKeepalive = 25;
     }];
-
-    # DNS
-    postSetup = ''resolvectl dns wg0 10.0.0.116'';
-    postDown = ''resolvectl revert wg0'';
   };
 }
