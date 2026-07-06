cff7aed352c7a3748e26010cbfad57d1e6d0aa64
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-06 22:17:56 +0200

    cinny: made title bar smaller
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- users/sakulflee/packages/social.nix
+++ users/sakulflee/packages/social.nix
@@ -4,4 +4,14 @@
     cinny-desktop
     element-desktop
   ];
+
+  # Setting x11 here makes the title bar like any other application and not be GIGANTIC.
+  xdg.desktopEntries.cinny-desktop = {
+    name = "Cinny";
+    exec = "env GDK_BACKEND=x11 cinny %U";
+    icon = "cinny";
+    terminal = false;
+    categories = [ "Network" "InstantMessaging" ];
+    mimeType = [ "x-scheme-handler/matrix" ];
+  };
 }
