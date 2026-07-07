11c37413de460642e3bca8e9480679acdac9bef4
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-07 22:14:57 +0200

    wireguard: trying with hostname as var
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- shared/nix/wireguard.nix
+++ shared/nix/wireguard.nix
@@ -1,5 +1,6 @@
 { pkgs, config, lib, ... }:
 let
+  serverPublicKey = "tDj18GzgpKzFsfu0JdltiienO5uvJDw1e4Hwq1551DI=";
   hostCfg = {
     "Evil-Donkey" = {
       address = [ "10.100.0.2/32" ];
@@ -9,15 +10,14 @@ let
     };
   };
   this = hostCfg.${config.networking.hostName} or (throw "wireguard.nix: unknown host ${config.networking.hostName}");
-  serverPublicKey = "tDj18GzgpKzFsfu0JdltiienO5uvJDw1e4Hwq1551DI=";
 in {
   sops.defaultSopsFile = ../../secrets.yaml;
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  sops.secrets."wireguard_admin_private_key" = { };
+  sops.secrets.${"wireguard_${config.networking.hostName}_private_key"} = { };
 
   networking.wg-quick.interfaces.wg0 = {
     address = this.address;
-    privateKeyFile = config.sops.secrets.wireguard_admin_private_key.path;
+    privateKeyFile = config.sops.secrets.${"wireguard_${config.networking.hostName}_private_key"}.path;
     dns = [ "10.0.0.116" ];
     table = null;
     peers = [{
