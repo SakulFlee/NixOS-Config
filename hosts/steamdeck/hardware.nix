2711b9861a44bfe536ac3b8d9c72b9ae7d231993
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-10 02:48:23 +0200

    steamdeck: updated hardware

--- hosts/steamdeck/hardware.nix
+++ hosts/steamdeck/hardware.nix
@@ -1,18 +1,36 @@
 { config, lib, pkgs, modulesPath, ... }: {
+  # Additional boot arguments should _mostly_ stay in the host config as they tend to highly customized between hosts
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
   # Disk config
-    fileSystems."/" =
-    { device = "/dev/disk/by-uuid/be9cbef6-0a9f-4a94-8627-766ea4cff995";
-      fsType = "ext4";
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/da954ab9-0559-4cf4-9004-2826a61078d1";
+      fsType = "btrfs";
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/da954ab9-0559-4cf4-9004-2826a61078d1";
+      fsType = "btrfs";
+      options = [ "subvol=home" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/da954ab9-0559-4cf4-9004-2826a61078d1";
+      fsType = "btrfs";
+      options = [ "subvol=nix" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/F28F-32D3";
+    { device = "/dev/disk/by-uuid/C78D-6801";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/bf363541-aef5-42b5-ac4b-316a76b262e0"; }
+    [ { device = "/dev/disk/by-uuid/f2aef7fb-e914-4284-8b8d-c920a56d2a4a"; }
     ];
 
   # Steam Hardware
