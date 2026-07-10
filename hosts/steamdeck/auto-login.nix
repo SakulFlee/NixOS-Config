31798a9f7d0de309ba595695dc9e1d3d87ed1799
Helm <helm@sakul-flee.de>
2026-07-10 15:51:00 +0000

    fix(steamdeck): custom session with sleep for DRM handoff
    - Create custom steamdeck.desktop session with a wrapper script
    - Add 2-second sleep before gamescope starts to let SDDM's greeter
      compositor fully release DRM master
    - Switch SDDM compositor to KWin for the greeter
    - Add IdleTimeout=0 to prevent screen blanking

--- hosts/steamdeck/auto-login.nix
+++ hosts/steamdeck/auto-login.nix
@@ -1,4 +1,25 @@
-{ lib, ... }: {
+{ lib, pkgs, ... }:
+
+let
+  steamdeck-session = pkgs.writeShellScriptBin "steamdeck-session" ''
+    # Give SDDM's greeter compositor time to fully release DRM master
+    # before gamescope tries to acquire it (avoids black screen on
+    # Steam Deck's AMD Van Gogh GPU with auto-login)
+    sleep 2
+    exec ${pkgs.gamescope}/bin/gamescope \
+      --steam \
+      --force-orientation right \
+      -- steam -tenfoot -pipewire-dmabuf
+  '';
+
+  steamdeck-desktop = pkgs.writeTextDir "share/wayland-sessions/steamdeck.desktop" ''
+    [Desktop Entry]
+    Name=SteamDeck
+    Comment=Steam Big Picture via Gamescope
+    Exec=${steamdeck-session}/bin/steamdeck-session
+    Type=Application
+  '';
+in {
   # Auto-login to Steam Big Picture.
   # SDDM's Relogin defaults to false, so exiting Steam drops you at the
   # greeter where you can select "Plasma" and enter your password for
@@ -8,10 +29,12 @@
       enable = true;
       user = "sakulflee";
     };
-    defaultSession = lib.mkForce "steam";
+    defaultSession = lib.mkForce "steamdeck";
     sddm = {
       wayland.compositor = "kwin";
       settings.General.IdleTimeout = 0;
     };
   };
+
+  environment.systemPackages = [ steamdeck-desktop ];
 }
