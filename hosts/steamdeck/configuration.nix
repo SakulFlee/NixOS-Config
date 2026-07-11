f164d05e914aecdeab5adad169e76a80722b7fed
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-11 20:32:21 +0200

    steamdeck: enabling real user config
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- hosts/steamdeck/configuration.nix
+++ hosts/steamdeck/configuration.nix
@@ -1,19 +1,14 @@
 { config, pkgs, inputs, ... }: {
   imports = [
-    ./hardware.nix
     inputs.jovian.nixosModules.jovian
+    ./hardware.nix
+    ../../users/_.nix
   ];
 
   nixpkgs.config.allowUnfree = true;
 
   boot.loader.systemd-boot.enable = true;
   networking.hostName = "SteamDeck";
-
-  users.users.sakulflee = {
-    isNormalUser = true;
-    extraGroups = [ "audio" "networkmanager" "video" "wheel" ];
-  };
-  security.sudo.wheelNeedsPassword = false;
 
   services.openssh = {
     enable = true;
