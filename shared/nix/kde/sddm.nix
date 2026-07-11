8a95deb3ed2bc86934bab0634e011e9424c85d0c
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-12 01:31:15 +0200

    sddm: removed theme
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- shared/nix/kde/sddm.nix
+++ shared/nix/kde/sddm.nix
@@ -1,17 +1,8 @@
 { lib, pkgs, ... }: {
-  # SDDM
-  environment.systemPackages = [
-    (pkgs.catppuccin-sddm.override {
-      flavor = "mocha";
-      accent = "mauve";
-    })
-  ];
-
   services.displayManager = {
     sddm = {
       enable = true;
       wayland.enable = true;
-      theme = "catppuccin-mocha-mauve";
     };
 
     # mkDefault so hosts (e.g. SteamDeck via Jovian) can override
