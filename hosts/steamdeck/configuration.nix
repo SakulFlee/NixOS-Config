c3bd2db174f3e368433beef046b8d24051508e2a
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-12 01:32:13 +0200

    steamdeck: cleanup
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- hosts/steamdeck/configuration.nix
+++ hosts/steamdeck/configuration.nix
@@ -7,18 +7,8 @@
     ../../users/_.nix
   ];
 
-  nixpkgs.config.allowUnfree = true;
-
   boot.loader.systemd-boot.enable = true;
   networking.hostName = "SteamDeck";
-
-  services.openssh = {
-    enable = true;
-    settings.PasswordAuthentication = true;
-  };
-
-  # Add Plasma as desktop session for "Switch to Desktop"
-  services.desktopManager.plasma6.enable = true;
 
   jovian = {
     devices.steamdeck.enable = true;
