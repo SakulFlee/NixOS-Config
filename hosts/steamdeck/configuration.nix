17b1baaa46572798b8af032a94552a7722eccd61
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-12 01:31:46 +0200

    steamdeck: simplified config now that the issue has been found
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- hosts/steamdeck/configuration.nix
+++ hosts/steamdeck/configuration.nix
@@ -3,29 +3,7 @@
     inputs.jovian.nixosModules.jovian
     ./hardware.nix
     ../../shared/nix-hardware/_.nix
-    ../../shared/nix/boot-loader.nix
-    ../../shared/nix/experimental-features.nix
-    ../../shared/nix/fonts.nix
-    ../../shared/nix/gpg.nix
-    ../../shared/nix/home-manager.nix
-    ../../shared/nix/kde/_.nix
-    ../../shared/nix/kernel.nix
-    ../../shared/nix/locale.nix
-    ../../shared/nix/mount-nas.nix
-    # ../../shared/nix/_.nix
-    ../../shared/nix/nixos-config-rebuilder.nix
-    ../../shared/nix/nixpkgs-unfree.nix
-    ../../shared/nix/printing.nix
-    ../../shared/nix/services/_.nix
-    ../../shared/nix/sops.nix
-    ../../shared/nix/ssh.nix
-    ../../shared/nix/steam.nix
-    ../../shared/nix/system-packages.nix
-    ../../shared/nix/system-state.nix
-    ../../shared/nix/wayland.nix
-    ../../shared/nix/wireguard.nix
-    ../../shared/nix/zram.nix
-    ../../shared/nix/zsh.nix
+    ../../shared/nix/_.nix
     ../../users/_.nix
   ];
 
