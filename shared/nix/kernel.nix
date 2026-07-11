d6fae29b88c23169af837a4b3182054fdb6197e6
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-11 21:16:16 +0200

    general: removed other kernels but ZEN
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- shared/nix/kernel.nix
+++ shared/nix/kernel.nix
@@ -1,16 +1,4 @@
 { lib, pkgs, ... }: {
   # Default kernel is gaming focused (ZEN)
   boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
-
-  specialisation = {
-    latest.configuration = {
-      system.nixos.tags = [ "Latest" ];
-      boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
-    };
-
-    lts.configuration = {
-      system.nixos.tags = [ "LTS" ];
-      boot.kernelPackages = lib.mkForce pkgs.linuxPackages; # Defaults to latest LTS
-    };
-  };
 }
