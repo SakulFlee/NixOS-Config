00f9e5b31f95247bf3f2df77172aa36f32df45c0
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-11 02:51:51 +0200

    syncthing: changes to use service
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- shared/nix/services/syncthing.nix
+++ shared/nix/services/syncthing.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }: {
   services.syncthing = {
     enable = true;
+    user = "sakulflee";
+    dataDir = "/home/sakulflee/";
+    configDir = "/home/sakulflee/.config/syncthing";
     guiAddress = "127.0.0.1:8384";
     openDefaultPorts = true;
     overrideDevices = false;
