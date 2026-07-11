5d40d52467b38b5b97b65649986d1d9c29f874fb
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-11 21:14:23 +0200

    steamdeck: trying to enable more of shared
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- hosts/steamdeck/configuration.nix
+++ hosts/steamdeck/configuration.nix
@@ -3,9 +3,29 @@
     inputs.jovian.nixosModules.jovian
     ./hardware.nix
     ../../shared/nix-hardware/firmware.nix
+    ../../shared/nix/boot-loader.nix
+    ../../shared/nix/experimental-features.nix
+    ../../shared/nix/fonts.nix
+    ../../shared/nix/gpg.nix
     ../../shared/nix/home-manager.nix
-    ../../shared/nix/zsh.nix
+    # ../../shared/nix/kde DIR
+    ../../shared/nix/kernel.nix
+    ../../shared/nix/locale.nix
+    ../../shared/nix/mount-nas.nix
+    # ../../shared/nix/_.nix
+    ../../shared/nix/nixos-config-rebuilder.nix
+    ../../shared/nix/nixpkgs-unfree.nix
+    ../../shared/nix/printing.nix
+    # ../../shared/nix/services DIR
     ../../shared/nix/sops.nix
+    ../../shared/nix/ssh.nix
+    # ../../shared/nix/steam.nix
+    ../../shared/nix/system-packages.nix
+    ../../shared/nix/system-state.nix
+    # ../../shared/nix/wayland.nix
+    ../../shared/nix/wireguard.nix
+    ../../shared/nix/zram.nix
+    ../../shared/nix/zsh.nix
     ../../users/_.nix
   ];
 
