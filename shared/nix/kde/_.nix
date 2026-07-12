4338c5d436e2d973c5b1ddbac95dbddfa44f4023
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-12 02:01:02 +0200

    kde: removes envs that may not be needed?
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- shared/nix/kde/_.nix
+++ shared/nix/kde/_.nix
@@ -1,6 +1,5 @@
 { ... }: {
   imports = [
-    ./envs.nix
     ./plasma.nix
     ./xdg-portal.nix
     ./sddm.nix
--- shared/nix/kde/envs.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, ... }: {
-  services.xserver.displayManager.sessionCommands = ''
-    ${pkgs.systemd}/bin/systemctl --user import-environment DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY WAYLAND_DISPLAY
-  '';
-}
