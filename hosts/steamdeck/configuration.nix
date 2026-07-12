2285b6898dc3c413b475b72a3533122906c89cd6
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-12 04:45:38 +0200

    Revert "steamdeck: enabled more services"
    This reverts commit d73cb1afdc304c7c51336c009cb26c2dcc19aec1.

--- hosts/steamdeck/configuration.nix
+++ hosts/steamdeck/configuration.nix
@@ -32,9 +32,9 @@
     # # ../../shared/nix/services/flatpak.nix
     ../../shared/nix/services/llama-swap.nix
     ../../shared/nix/services/ollama.nix
-    ../../shared/nix/services/openrgb.nix
-    ../../shared/nix/services/open-webui.nix
-    ../../shared/nix/services/sunshine.nix
+    # ../../shared/nix/services/openrgb.nix
+    # ../../shared/nix/services/open-webui.nix
+    # ../../shared/nix/services/sunshine.nix
     # ../../shared/nix/services/syncthing.nix
 
     ../../users/_.nix
