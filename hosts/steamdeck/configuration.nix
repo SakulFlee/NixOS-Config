65f94a61361cedc1777a19a9e384d6d6fb73090d
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-12 18:58:55 +0200

    steamdeck: added open-webui service
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- hosts/steamdeck/configuration.nix
+++ hosts/steamdeck/configuration.nix
@@ -33,7 +33,7 @@
     ../../shared/nix/services/llama-swap.nix
     ../../shared/nix/services/ollama.nix
     ../../shared/nix/services/openrgb.nix
-    # ../../shared/nix/services/open-webui.nix
+    ../../shared/nix/services/open-webui.nix
     # ../../shared/nix/services/sunshine.nix
     # ../../shared/nix/services/syncthing.nix
 
