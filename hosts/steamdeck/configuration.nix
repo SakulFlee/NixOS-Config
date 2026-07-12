d940e6e4519fbb5b55143cebfbe0bc66905fa4b2
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-12 03:24:22 +0200

    temp: removing dbus
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- hosts/steamdeck/configuration.nix
+++ hosts/steamdeck/configuration.nix
@@ -27,7 +27,7 @@
     ../../shared/nix/zsh.nix
 
     ../../shared/nix/services/avahi.nix
-    ../../shared/nix/services/dbus.nix
+    # ../../shared/nix/services/dbus.nix
     # ../../shared/nix/services/flatpak.nix
     ../../shared/nix/services/llama-swap.nix
     ../../shared/nix/services/ollama.nix
