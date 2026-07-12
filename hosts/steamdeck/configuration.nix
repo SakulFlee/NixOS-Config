2388eb14cfbb0098d0ae04c76ef499bd877cfe1e
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-12 02:36:56 +0200

    Stepping back again
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- hosts/steamdeck/configuration.nix
+++ hosts/steamdeck/configuration.nix
@@ -2,7 +2,30 @@
   imports = [
     inputs.jovian.nixosModules.jovian
     ./hardware.nix
-    ../../shared/nix/_.nix
+    # ../../shared/nix/_.nix
+    ../../shared/boot-loader.nix
+    ../../shared/experimental-features.nix
+    ../../shared/fonts.nix
+    ../../shared/gpg.nix
+    ../../shared/home-manager.nix
+    # ../../shared/kde
+    ../../shared/kernel.nix
+    ../../shared/locale.nix
+    ../../shared/mount-nas.nix
+    # ../../shared/_.nix
+    ../../shared/nixos-config-rebuilder.nix
+    ../../shared/nixpkgs-unfree.nix
+    ../../shared/printing.nix
+    # ../../shared/services
+    ../../shared/sops.nix
+    ../../shared/ssh.nix
+    ../../shared/steam.nix
+    ../../shared/system-packages.nix
+    ../../shared/system-state.nix
+    ../../shared/wayland.nix
+    ../../shared/wireguard.nix
+    ../../shared/zram.nix
+    ../../shared/zsh.nix
     ../../users/_.nix
   ];
 
