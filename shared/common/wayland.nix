cbdd0d19168f5a50c503d0c8e305ee2adcc607a7
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-13 18:10:13 +0200

    kde/sddm: attempting to fix language

--- shared/common/wayland.nix
+++ shared/common/wayland.nix
@@ -1,8 +1,5 @@
 { ... }:
 {
-  # Wayland is enabled by disabling x11/xserver
-  services.xserver.enable = false;
-
-    # Hint Electron apps to use Wayland
+  # Hint Electron apps to use Wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
