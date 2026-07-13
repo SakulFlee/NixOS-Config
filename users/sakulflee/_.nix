f12cf2f2472587e3cfd15e335373276dd2047bbf
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-13 02:00:49 +0200

    Missing config block
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- users/sakulflee/_.nix
+++ users/sakulflee/_.nix
@@ -5,47 +5,49 @@
     description = "Install user packages (mostly GUI, requires GUI) for sakulflee";
   };
 
-  users.users.sakulflee = {
-    description = "SakulFlee";
+  config = {
+    users.users.sakulflee = {
+      description = "SakulFlee";
 
-    initialPassword = "nixos";
+      initialPassword = "nixos";
 
-    isNormalUser = true;
-    
-    extraGroups = [ 
-      # To be able to use network manager (KDE Networking)
-      "networkmanager"
-      # Sudo privileges
-      "wheel"
-      # Access to GPU (also required by Sunshine)
-      "video" 
-      "render"
-      # Universal inputs (gamepads, touch, mouse, keyboard, etc.; Also required for Sunshine!)
-      "uinput"
-      # i2c support
-      "i2c"
-    ];
+      isNormalUser = true;
 
-    shell = pkgs.zsh;
-  };
+      extraGroups = [ 
+        # To be able to use network manager (KDE Networking)
+        "networkmanager"
+        # Sudo privileges
+        "wheel"
+        # Access to GPU (also required by Sunshine)
+        "video" 
+        "render"
+        # Universal inputs (gamepads, touch, mouse, keyboard, etc.; Also required for Sunshine!)
+        "uinput"
+        # i2c support
+        "i2c"
+      ];
 
-  home-manager.users.sakulflee = {
-    imports = [
-      ../../shared/home-manager/_.nix
-      ./git.nix
-      ./gpg.nix
-      ./zsh.nix
-    ] ++ (if config.custom.installPackages then [ 
-      ./packages/_.nix
-      ./configs/_.nix
-    ] else [ ]);
-
-    home.pointerCursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Classic";
-      size = 24;
+      shell = pkgs.zsh;
     };
 
-    home.stateVersion = "25.11";
+    home-manager.users.sakulflee = {
+      imports = [
+        ../../shared/home-manager/_.nix
+        ./git.nix
+        ./gpg.nix
+        ./zsh.nix
+      ] ++ (if config.custom.installPackages then [ 
+        ./packages/_.nix
+        ./configs/_.nix
+      ] else [ ]);
+
+      home.pointerCursor = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Classic";
+        size = 24;
+      };
+
+      home.stateVersion = "25.11";
+    };
   };
 }
