2d488504198d830ff38b47424d5972752672dbb1
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-14 00:44:58 +0200

    bridges: fix
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- hosts/homelab/services/mautrix-discord.nix
+++ hosts/homelab/services/mautrix-discord.nix
@@ -5,6 +5,10 @@
     enable = true;
     environmentFile = config.sops.secrets."mautrix-discord-env".path;
     settings = {
+      homeserver = {
+        address = "http://127.0.0.1:6167";
+        domain = "sakul-flee.de";
+      };
       bridge = {
         displayname_check = false;
         federation = true;
--- hosts/homelab/services/mautrix-whatsapp.nix
+++ hosts/homelab/services/mautrix-whatsapp.nix
@@ -5,6 +5,10 @@
     enable = true;
     environmentFile = config.sops.secrets."mautrix-whatsapp-env".path;
     settings = {
+      homeserver = {
+        address = "http://127.0.0.1:6167";
+        domain = "sakul-flee.de";
+      };
       bridge = {
         double_puppet_server_url = "https://matrix.sakul-flee.de";
         displayname_check = false;
