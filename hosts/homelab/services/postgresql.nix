aafec9656651f07b4b1e0c9a95e150c403c41f73
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-14 03:23:57 +0200

    simplified database passwords
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- hosts/homelab/services/postgresql.nix
+++ hosts/homelab/services/postgresql.nix
@@ -1,6 +1,4 @@
 { config, pkgs, lib, ... }: {
-  sops.secrets.woodpecker_db_password = {};
-
   services.postgresql = {
     enable = true;
     ensureDatabases = [ "forgejo" "woodpecker" "bitmagnet" ];
@@ -21,8 +19,8 @@
     authentication = ''
       local forgejo forgejo peer map=forgejo-map
       local woodpecker woodpecker peer map=woodpecker-map
-      host woodpecker woodpecker 127.0.0.1/32 scram-sha-256
-      host woodpecker woodpecker ::1/128 scram-sha-256
+      host woodpecker woodpecker 127.0.0.1/32 trust
+      host woodpecker woodpecker ::1/128 trust
       host bitmagnet bitmagnet 127.0.0.1/32 scram-sha-256
       host bitmagnet bitmagnet ::1/128 scram-sha-256
       host bitmagnet bitmagnet 10.88.0.0/16 scram-sha-256
@@ -40,8 +38,6 @@
 
   systemd.services.postgresql = {
     postStart = lib.mkAfter ''
-      WPW=$(cat ${config.sops.secrets.woodpecker_db_password.path})
-      psql -d woodpecker -c "ALTER USER woodpecker WITH PASSWORD '$WPW';" 2>/dev/null || true
       psql -d bitmagnet -c "ALTER USER bitmagnet WITH PASSWORD 'bitmagnet';" 2>/dev/null || true
     '';
   };
