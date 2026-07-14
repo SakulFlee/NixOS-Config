d833b50056a0858bd5b72b3f5b22170e3699a55f
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-14 21:34:21 +0200

    homelab: adjusted disk IDs that magically suddenly changed?!

--- hosts/homelab/hardware.nix
+++ hosts/homelab/hardware.nix
@@ -13,30 +13,30 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1be6133a-ae81-40b3-94ae-e30195b638b8";
+    { device = "/dev/disk/by-uuid/f947dbe8-fde3-4002-92bf-fee906abab73";
       fsType = "btrfs";
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/1be6133a-ae81-40b3-94ae-e30195b638b8";
+    { device = "/dev/disk/by-uuid/f947dbe8-fde3-4002-92bf-fee906abab73";
       fsType = "btrfs";
       options = [ "subvol=home" ];
     };
 
   fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/1be6133a-ae81-40b3-94ae-e30195b638b8";
+    { device = "/dev/disk/by-uuid/f947dbe8-fde3-4002-92bf-fee906abab73";
       fsType = "btrfs";
       options = [ "subvol=nix" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D9DF-0705";
+    { device = "/dev/disk/by-uuid/5B72-9486";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/22ab5fe2-7d6b-4ddf-8296-acec80688802"; }
+    [ { device = "/dev/disk/by-uuid/848a85dc-c812-478a-81fb-9e35926915b5"; }
     ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
