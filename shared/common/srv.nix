36f1bf2b3f6018aa607c3b39f82411431f4f1aeb
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-16 02:28:00 +0200

    srv: updated order
    Signed-off-by: @SakulFlee | Lukas Weber <dev@sakul-flee.de>

--- shared/common/srv.nix
+++ shared/common/srv.nix
@@ -16,20 +16,8 @@ let
     SERVICE="$1"
     CMD="''${2:-start}"
 
-    # Kill any previous journalctl -f for the same service
-    pkill -f "journalctl -u $SERVICE -f" 2>/dev/null || true
-
-    # Tail journal in background
-    journalctl -u "$SERVICE" -f --no-hostname -o short &
-    JPID=$!
-
-    # Run the systemctl command
-    systemctl "$CMD" "$SERVICE"
-    EC=$?
-
-    # Kill journal tail
-    kill $JPID 2>/dev/null || true
-    exit $EC
+    systemctl "$CMD" "$SERVICE" &
+    journalctl -u "$SERVICE" -f
   '';
 in {
   environment.systemPackages = [ srv ];
