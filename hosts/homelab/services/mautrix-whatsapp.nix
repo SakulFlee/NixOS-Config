fd1ad3cac1a0be694ab77a9258dce1cbf0f670ae
Helm <helm@homelab>
2026-07-16 04:57:02 +0200

    fix: mautrix, not matrix

--- hosts/homelab/services/mautrix-whatsapp.nix
+++ hosts/homelab/services/mautrix-whatsapp.nix
@@ -8,10 +8,10 @@
     SystemCallFilter = [];
   };
 
-  sops.secrets."matrix-whatsapp-env" = {};
+  sops.secrets."mautrix-whatsapp-env" = {};
   services.mautrix-whatsapp = {
     enable = true;
-    environmentFile = config.sops.secrets."matrix-whatsapp-env".path;
+    environmentFile = config.sops.secrets."mautrix-whatsapp-env".path;
     settings = {
       homeserver = {
         address = "http://127.0.0.1:6167";
