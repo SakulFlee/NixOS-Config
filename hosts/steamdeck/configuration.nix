2f999fe637d92b87ed8643ec1238489175b6c445
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-07-20 00:05:11 +0200

    steamdeck: use full Jovian NixOS module for gaming session, power button, and desktop switching

--- hosts/steamdeck/configuration.nix
+++ hosts/steamdeck/configuration.nix
@@ -1,64 +1,25 @@
-{ config, pkgs, lib, ... }:
-let
-  gamescopeSession = pkgs.runCommand "gamescope-session" {
-    providedSessions = [ "gamescope" ];
-  } ''
-    mkdir -p $out/share/wayland-sessions
-    cat > $out/share/wayland-sessions/gamescope.desktop <<EOF
-    [Desktop Entry]
-    Name=Gamescope
-    Comment=Steam Gaming Mode
-    Exec=${pkgs.gamescope}/bin/gamescope -e -- ${pkgs.steam}/bin/steam -gamepadui
-    Type=Application
-    EOF
-  '';
-
-  steamosDbusExec = pkgs.writeShellScriptBin "steamos-dbus-exec" ''
-    exec loginctl terminate-user "$(id -un)"
-  '';
-
-  steamosDbusService = pkgs.writeTextDir "share/dbus-1/services/com.steampowered.SteamOSManager1.service" ''
-    [D-BUS Service]
-    Name=com.steampowered.SteamOSManager1
-    Exec=${steamosDbusExec}/bin/steamos-dbus-exec
-  '';
-in {
+{ config, pkgs, lib, inputs, ... }:
+{
   imports = [
+    inputs.jovian.nixosModules.default
     ./hardware.nix
     ../../shared/common/_.nix
     ../../users/_.nix
   ];
 
-  networking.hostName = "SteamDeck";
-
-  services.displayManager = {
-    autoLogin = {
-      enable = true;
-      user = "sakulflee";
-    };
-    defaultSession = "gamescope";
-    sessionPackages = [ gamescopeSession ];
+  jovian.steam = {
+    enable = true;
+    autoStart = true;
+    user = "sakulflee";
+    desktopSession = "plasma";
   };
 
-  services.logind.settings.Login.HandlePowerKey = "suspend";
-
-  services.dbus.packages = [ steamosDbusService ];
+  networking.hostName = "SteamDeck";
 
   programs.gamescope = {
     enable = true;
     capSysNice = false;
   };
-
-  environment.systemPackages = with pkgs; [
-    (makeDesktopItem {
-      name = "steam-gaming-mode";
-      exec = "${steam}/bin/steam -gamepadui";
-      icon = "steam";
-      desktopName = "Return to Gaming Mode";
-      comment = "Switch to Steam Game Mode";
-      categories = [ "Game" ];
-    })
-  ];
 
   home-manager.users.sakulflee = {
     programs.plasma.configFile."kwinrulesrc" = {
