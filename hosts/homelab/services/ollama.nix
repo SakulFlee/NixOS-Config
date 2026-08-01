96775b1512e782f32f3fe5150bfd4fc87e5322d5
@SakulFlee | Lukas Weber <dev@sakul-flee.de>
2026-08-01 17:56:38 +0200

    homelab: ollama: different brackets

--- hosts/homelab/services/ollama.nix
+++ hosts/homelab/services/ollama.nix
@@ -2,9 +2,9 @@
   services.ollama = {
     enable = true;
     package = pkgs.ollama-vulkan;
-    environmentVariables = [
-      "OLLAMA_IGPU_ENABLE=1"
-    ];
+    environmentVariables = {
+      OLLAMA_IGPU_ENABLE = "1";
+    };
   };
 
   systemd.services.ollama.serviceConfig = {
